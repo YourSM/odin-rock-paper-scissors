127c67e316168173a74c9ae463ba943dc5810574
YourSM <156563558+YourSM@users.noreply.github.com>
2024-06-27 14:24:57 -0700

    Create logic for computer choice

---
+++ javascript.js
@@ -0,0 +1,28 @@
+function getComputerChoice() {
+  let x = Math.random();
+  if (x >= 0 && x <= 0.3) {
+    y = "Rock";
+  } else if (x >= 0.4 && x <= 0.6) {
+    y = "Scissors";
+    } else y = "Paper";
+    alert(y)
+  }
+
+getComputerChoice()
+
+
+// create function getComputerChoice
+// Get getComputerChoice to return one of three options
+// Math.Random() * 10.
+// Use math.Random to create numbers 0-1
+  // split results three ways and assign them to each option.
+
+
+// function getComputerChoice()
+  // x = Math.Random()*10
+  // if x >= 0 && x <= 0.3 {
+    // y = "Rock"
+  // } else if x >= 0.50 && <= 0.6 {
+    // y = "Scissors"
+    // } else y = "Paper"
+    // console.log(y) 
