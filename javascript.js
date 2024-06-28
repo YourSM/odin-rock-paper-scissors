82c09dbf8a7bc3271272b720b15d25197481c11b
YourSM <156563558+YourSM@users.noreply.github.com>
2024-06-28 12:25:19 -0700

    Create logic for human choice

--- javascript.js
+++ javascript.js
@@ -1,28 +1,22 @@
-function getComputerChoice() {
+function getComputerChoice(y) {
   let x = Math.random();
   if (x >= 0 && x <= 0.3) {
     y = "Rock";
   } else if (x >= 0.4 && x <= 0.6) {
     y = "Scissors";
     } else y = "Paper";
-    alert(y)
+    console.log(y)
   }
 
 getComputerChoice()
 
+function getHumanChoice() {
+  humanChoice = prompt('Please choose between "Rock" "Paper" and "Scissors"');
+  if (humanChoice !== "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors") {
+    getComputerChoice(humanChoice);
+  } else {
+      console.log(humanChoice)
+  }
+}
 
-// create function getComputerChoice
-// Get getComputerChoice to return one of three options
-// Math.Random() * 10.
-// Use math.Random to create numbers 0-1
-  // split results three ways and assign them to each option.
-
-
-// function getComputerChoice()
-  // x = Math.Random()*10
-  // if x >= 0 && x <= 0.3 {
-    // y = "Rock"
-  // } else if x >= 0.50 && <= 0.6 {
-    // y = "Scissors"
-    // } else y = "Paper"
-    // console.log(y) 
+  getHumanChoice()
