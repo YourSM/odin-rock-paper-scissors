921e157a256d8725c6698f9d475a0457db8ca8cc
YourSM <156563558+YourSM@users.noreply.github.com>
2024-06-30 12:35:49 -0700

    Fix getHumanChoice randomizing appropriate inputs
    - Making inputs lowercase allows user to input any variation of capitals
      without randomizing the input.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
+getHumanChoice()
+getComputerChoice()
 function getComputerChoice(y) {
   let x = Math.random();
   if (x >= 0 && x <= 0.3) {
-    y = "Rock";
+    y = "rock";
   } else if (x >= 0.4 && x <= 0.6) {
-    y = "Scissors";
-    } else y = "Paper";
+    y = "scissors";
+    } else y = "paper";
     console.log(y)
   }
 
-getComputerChoice()
-
 function getHumanChoice() {
-  humanChoice = prompt('Please choose between "Rock" "Paper" and "Scissors"');
-  if (humanChoice !== "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors") {
-    getComputerChoice(humanChoice);
+  let humanChoice = prompt('Please choose between "Rock" "Paper" and "Scissors"');
+  if (humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors") {
+    console.log(humanChoice.toLowerCase());
   } else {
-      console.log(humanChoice)
+      getComputerChoice(humanChoice);
+    
   }
 }
-
-  getHumanChoice()
