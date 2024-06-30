b68105b6b3432e5c3fee3c803d51d3d0be96d868
YourSM <156563558+YourSM@users.noreply.github.com>
2024-06-30 13:13:24 -0700

    Create logic for one round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
-getHumanChoice()
-getComputerChoice()
+let humanScore = 0
+let computerScore = 0
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
 function getComputerChoice(y) {
   let x = Math.random();
   if (x >= 0 && x <= 0.3) {
@@ -7,15 +13,33 @@ function getComputerChoice(y) {
   } else if (x >= 0.4 && x <= 0.6) {
     y = "scissors";
     } else y = "paper";
-    console.log(y)
+   return(y)
   }
 
 function getHumanChoice() {
   let humanChoice = prompt('Please choose between "Rock" "Paper" and "Scissors"');
   if (humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors") {
-    console.log(humanChoice.toLowerCase());
+    return(humanChoice.toLowerCase());
   } else {
-      getComputerChoice(humanChoice);
-    
+      console.log("You didn\'t choose an appropriate value, so one was assigned!");
+      return(getComputerChoice(humanChoice));
+  }
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lost! Paper beats rock!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You won! Rock beats scissors!");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lost! Scissors beats paper!");
+  } else  if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You won! Paper beats rock!");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You won! Scissors beats paper!");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lost! Rock beats scissors!")
+  } else {
+      console.log("You tied as both sides choose " + humanSelection + "! No points were assigned!");
   }
 }
